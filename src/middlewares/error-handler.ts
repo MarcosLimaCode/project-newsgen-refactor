d97e9f802eb6060a3a43198d5b75e48f59ef84b0
Marcos Lima <mrsk1414@gmail.com>
2025-10-14 18:12:48 -0300

    refactor: errors validation

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -3,30 +3,26 @@ import { Request, Response, NextFunction } from "express";
 import httpStatus from "http-status";
 
 type AppError = Error & {
-  type: string
-}
+  type: string;
+};
 
 export default function errorHandlingMiddleware(
   error: Error | AppError,
   req: Request,
   res: Response,
-  next: NextFunction) {
-
+  next: NextFunction
+) {
   console.log(error);
 
-  const { name, message } = error;
-  if (name === "NotFound") {
-    return res.status(httpStatus.NOT_FOUND).send(message);
-  } else if (name === "Conflict") {
-    return res.status(httpStatus.CONFLICT).send(message);
-  } else if (name === "BadRequest") {
-    return res.status(httpStatus.BAD_REQUEST).send(message);
-  } else if (name === "UnprocessableEntity") {
-    return res.status(httpStatus.UNPROCESSABLE_ENTITY).send(message);
-  } else if (name === "Forbidden") {
-    return res.status(httpStatus.FORBIDDEN).send(message);
-  } else {
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR);
-  }
+  const errors = {
+    NotFound: httpStatus.NOT_FOUND,
+    Conflict: httpStatus.CONFLICT,
+    BadRequest: httpStatus.BAD_REQUEST,
+    UnprocessableEntity: httpStatus.UNPROCESSABLE_ENTITY,
+    Forbidden: httpStatus.FORBIDDEN,
+  };
 
+  const status = errors[error.name] || httpStatus.INTERNAL_SERVER_ERROR;
+
+  return res.status(status).send(error.message);
 }
