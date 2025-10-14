3b5245225d9853cfa5fc4481e531b4ed5d078ab8
Marcos Lima <mrsk1414@gmail.com>
2025-10-14 15:26:20 -0300

    refactor: rename functions

--- src/services/news-service.ts
+++ src/services/news-service.ts
@@ -3,16 +3,16 @@ import * as newsRepository from "../repositories/news-repository";
 import { AlterNewsData, CreateNewsData } from "../repositories/news-repository";
 
 export async function getNews() {
-  return newsRepository.getNoticias();
+  return newsRepository.getNews();
 }
 
 export async function getSpecificNews(id: number) {
-  const news = await newsRepository.getNoticiaById(id);
+  const news = await newsRepository.getNewsById(id);
   if (!news) {
     throw {
       name: "NotFound",
-      message: `News with id ${id} not found.`
-    }
+      message: `News with id ${id} not found.`,
+    };
   }
 
   return news;
@@ -20,33 +20,33 @@ export async function getSpecificNews(id: number) {
 
 export async function createNews(newsData: CreateNewsData) {
   await validate(newsData);
-  return newsRepository.createNoticia(newsData);
+  return newsRepository.createNews(newsData);
 }
 
 export async function alterNews(id: number, newsData: AlterNewsData) {
   const news = await getSpecificNews(id);
   await validate(newsData, news.title !== newsData.title);
 
-  return newsRepository.updateNoticia(id, newsData);
+  return newsRepository.updateNews(id, newsData);
 }
 
 export async function deleteNews(id: number) {
   await getSpecificNews(id);
-  return newsRepository.removeNoticia(id);
+  return newsRepository.removeNews(id);
 }
 
 async function validate(newsData: CreateNewsData, isNew = true) {
   // validate if news with specific text already exists
   if (isNew) {
     const newsWithTitle = await prisma.news.findFirst({
-      where: { title: newsData.title }
+      where: { title: newsData.title },
     });
 
     if (newsWithTitle) {
       throw {
         name: "Conflict",
-        message: `News with title ${newsData.title} already exist`
-      }
+        message: `News with title ${newsData.title} already exist`,
+      };
     }
   }
 
